a8df2680b18e6c66a4e83d44a0fc68de6046ff47
lgausman1 <l.gausman@comcast.net>
2015-06-17 15:58:11 -0700

    updating to latest remote dev

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
+  
+
+    context 'Validation' do
+    let(:user) do
+      #create a user in active memory
+      User.new({
+        email: "bana@na.com",
+        password: "adsf1234",
+        password_confirmation: "adsf1234"
+      })
+    end
+    it "validates presence of password_digest" do
+      #clear values of password & password_confirmation
+      user.password_digest = nil
+      expect(user).not_to be_valid
+    end
+
+    it "validates presence of email" do
+      #clear values of email
+      user.email = nil
+      expect(user).not_to be_valid
+    end
+
+    it "validates password & password confirmation match" do
+      user.password_confirmation = "not the same"
+      expect(user).not_to be_valid
+    end
+  end
+  
+
   pending "add some examples to (or delete) #{__FILE__}"
 end
