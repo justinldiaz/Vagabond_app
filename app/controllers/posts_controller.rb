61eb3a9840d1a329d3718f477fa2d951a62d2027
ratacat <neuralsplash@gmail.com>
2015-06-17 15:44:47 -0700

    must be logged in to access users/cities/posts methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
+	before_action :logged_in?
 
 	# posts GET    /posts(.:format)          posts#index
 	def index
 		@posts = Post.all
-		authorize @posts, :index?
 		render :index
 	end
 
