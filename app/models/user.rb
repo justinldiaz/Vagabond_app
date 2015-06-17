532d1b622a7dbd82b6f3e9741a57406965600f4f
ratacat <neuralsplash@gmail.com>
2015-06-16 17:57:13 -0700

    password confirmation and flash error working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
 	has_secure_password
+	#validates :password, confirmation: true
+	#validates :password_confirmation, presence: true
+	validates :email, presence: true
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+
 	include Gravtastic
 	gravtastic
 	belongs_to :city
