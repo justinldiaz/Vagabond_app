c8b50e432a9af61df14120d244837462d5080877
ratacat <neuralsplash@gmail.com>
2015-06-18 09:42:56 -0700

    users be able to see city#show, city#index, user#show without logging in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,7 +6,11 @@ module SessionsHelper
 	end
 
 	def current_user
-		@current_user ||= User.find(session[:user_id])
+		if session[:user_id] 
+			@current_user ||= User.find(session[:user_id])
+		else 
+			@current_user = nil
+		end
 	end
 
 	def logged_in?
