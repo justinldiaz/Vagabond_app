be290b98b7ad9e9a10b42b5b4193e8caa20241d8
jmbrenne <jeremybrenner0@gmail.com>
2015-06-18 13:53:30 -0700

    Integrated google maps API on citiy view

---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,26 @@
+
+   <script type="text/javascript"> 
+
+   var address = 'London, UK';
+
+   var map = new google.maps.Map(document.getElementById('map'), { 
+       mapTypeId: google.maps.MapTypeId.TERRAIN,
+       zoom: 6
+   });
+
+   var geocoder = new google.maps.Geocoder();
+
+   geocoder.geocode({
+      'address': address
+   }, 
+   function(results, status) {
+      if(status == google.maps.GeocoderStatus.OK) {
+         new google.maps.Marker({
+            position: results[0].geometry.location,
+            map: map
+         });
+         map.setCenter(results[0].geometry.location);
+      }
+   });
+
+   </script> 
